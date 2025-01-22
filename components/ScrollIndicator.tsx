8d5ff37d713d81aef69ba5062107b200b4565345
Caique Ribeiro de Oliveira <caiquer.eds@gmail.com>
2025-01-22 20:51:04 -0300

    Refactor ScrollIndicator component styling for improved layout

--- components/ScrollIndicator.tsx
+++ components/ScrollIndicator.tsx
@@ -4,7 +4,7 @@ import { FaChevronDown } from "react-icons/fa";
 export function ScrollIndicator() {
   return (
     <motion.div
-      className="absolute bottom-8 left-1/2 -translate-x-1/2 text-center"
+      className="absolute bottom-8"
       initial={{ opacity: 0, y: -20 }}
       animate={{
         opacity: [0, 1, 1, 0],
